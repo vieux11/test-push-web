15815d7b628767a18aa390d42b8ea5c1b9d7ba67
Berdin Matondo <berdinmatondo7@gmail.com>
2026-01-14 13:34:49 +0100

    firebase config refactor

---
+++ firebase-config-sw.example.js
@@ -0,0 +1,12 @@
+// Configuration Firebase pour le Service Worker (exemple)
+// Copiez ce fichier vers firebase-config-sw.js et remplissez avec vos vraies valeurs
+// firebase-config-sw.js est dans .gitignore et ne sera PAS commité
+
+const FIREBASE_CONFIG_SW = {
+  apiKey: "VOTRE_API_KEY_FIREBASE",
+  authDomain: "votre-projet.firebaseapp.com",
+  projectId: "votre-projet",
+  storageBucket: "votre-projet.firebasestorage.app",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abcdef123456"
+};
