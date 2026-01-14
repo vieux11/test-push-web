00f30d339b31a3c55f2baad5823e2365351c2aef
Berdin Matondo <berdinmatondo7@gmail.com>
2026-01-14 13:33:54 +0100

    modele config

---
+++ config.example.js
@@ -0,0 +1,21 @@
+// Fichier de configuration d'exemple
+// Copiez ce fichier vers config.js et remplissez avec vos vraies valeurs
+// config.js est dans .gitignore et ne sera PAS commité
+
+window.APP_CONFIG = {
+  // Configuration Firebase
+  firebase: {
+    apiKey: "VOTRE_API_KEY_FIREBASE",
+    authDomain: "votre-projet.firebaseapp.com",
+    projectId: "votre-projet",
+    storageBucket: "votre-projet.firebasestorage.app",
+    messagingSenderId: "123456789012",
+    appId: "1:123456789012:web:abcdef123456"
+  },
+  
+  // Clé VAPID pour les notifications push
+  vapidKey: "VOTRE_CLE_VAPID_ICI",
+  
+  // URL de l'API backend
+  apiBaseUrl: "http://localhost:3200/api/nzo-notification"
+};
